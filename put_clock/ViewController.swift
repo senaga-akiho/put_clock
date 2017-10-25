122f2485d2e1e6f230c19d0a8549e3f558bbc976
瀬長顕穂 <senri7@senagatakeshiho-no-MacBook-Pro.local>
2017-10-25 11:12:00 +0900

    時刻表示

--- put_clock/ViewController.swift
+++ put_clock/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var time_label: UILabel!
+    @IBOutlet weak var date_label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // 1秒ごとに「displayClock」を実行する
+        let timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(displayClock), userInfo: nil, repeats: true)
+        timer.fire()    // 無くても動くけどこれが無いと初回の実行がラグる
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // 現在時刻を表示する処理
+    @objc func displayClock() {
+        // 現在時刻を「HH:MM:SS」形式で取得する
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy:MM:dd:HH:mm:ss"
+        var displayTime = formatter.string(from: Date())    // Date()だけで現在時刻を表す
+        
+        // 0から始まる時刻の場合は「 H:MM:SS」形式にする
+        if displayTime.hasPrefix("0") {
+            // 最初に見つかった0だけ削除(スペース埋め)される
+            if let range = displayTime.range(of: "0") {
+                displayTime.replaceSubrange(range, with: " ")
+            }
+        }
+        // ラベルに表示
+        time_label.text = displayTime
+        time_label.sizeToFit()
+        
+    }
 
 }
 
