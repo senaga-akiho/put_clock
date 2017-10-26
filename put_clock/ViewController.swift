0c0a70fee98afadc987b04fc1ecdc54dde913f15
瀬長顕穂 <senri7@senagatakeshiho-no-MacBook-Pro.local>
2017-10-26 18:49:27 +0900

    イベントの取得後配列に入ってるラベルに文字列書き換え，アプリアクティブ後にもイベント取得

--- put_clock/ViewController.swift
+++ put_clock/ViewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
+import EventKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var time_label: UILabel!
     @IBOutlet weak var date_label: UILabel!
-    
+    @IBOutlet weak var event1: UILabel!
+    @IBOutlet weak var event2: UILabel!
+    @IBOutlet weak var event3: UILabel!
+    var labelArray = [UILabel(), UILabel(), UILabel()]
+    private let myEventStore:EKEventStore = EKEventStore()
     override func viewDidLoad() {
+        accessApplication()
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        labelArray[0] = event1
+        labelArray[1] = event2
+        labelArray[2] = event3
+        print("通る")
+        //アプリがアクティブになった瞬間に呼び出す
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(
+            self,
+            selector: #selector(eventGet),
+            name:NSNotification.Name.UIApplicationDidBecomeActive,
+            object: nil)
+        eventGet()
         // 1秒ごとに「displayClock」を実行する
         let timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(displayClock), userInfo: nil, repeats: true)
         timer.fire()    // 無くても動くけどこれが無いと初回の実行がラグる
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -43,6 +59,56 @@ class ViewController: UIViewController {
         time_label.text = displayTime
         time_label.sizeToFit()
         
+    }
+    // アクセス権申請
+    func accessApplication()
+    {
+        // カレンダー追加の権限ステータスを取得
+        let authStatus = EKEventStore.authorizationStatus(for: .event)
+        
+        switch authStatus {
+        case .authorized: break
+        case .denied: break
+        case .restricted: break
+        case .notDetermined:
+            self.myEventStore.requestAccess(to: .event, completion: { (result:Bool, error:Error?) in
+                if result {
+//                    self.eventGet()
+                } else {
+                    // 使用拒否
+                }
+            })
+        }
+    }
+    
+    /*
+     Buttonが押されたときに呼ばれるメソッド.
+     */
+    @objc func eventGet() {
+        print("通ってる")
+        // イベントストアのインスタンスメソッドで述語を生成.
+        var predicate = NSPredicate()
+        // ユーザーの全てのカレンダーからフェッチせよ
+        predicate = myEventStore.predicateForEvents(withStart: Date()-60*60*24,
+                                                    end: Date()+60*60*24,
+                                                    calendars: nil)
+        // 述語にマッチする全てのイベントをフェッチ.
+        let events = myEventStore.events(matching: predicate)
+        print(type(of: events))
+        // イベントが見つかった.
+        
+        if !events.isEmpty {
+            var w=0
+            for i in events{
+                labelArray[w].text = i.title
+                print(i.title)
+//                print(i.startDate)
+//                print(i.endDate)
+                w+=1
+            }
+        }else{
+            print("何もない")
+        }
     }
 
 }
